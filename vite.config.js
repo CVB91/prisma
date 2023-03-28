8c5eb42fd23dabb9ec64f034fd2ced4d1a19c1cf
Christian <cphvb91@gmail.com>
2023-03-28 21:42:40 +0200

    new build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
-const root = resolve(__dirname, '/src')
+const root = resolve(__dirname, 'src')
 const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
@@ -14,7 +14,7 @@ export default defineConfig({
         main: resolve(root, 'index.html'),
         about: resolve(root, 'About', 'about.html'),
         gallery: resolve(root, 'gallery', 'gallery.html'),
-        contact: resolve(root, 'Contact', 'collections.html'),
+        contact: resolve(root, 'Contact', 'contact.html'),
       },
     },
   },

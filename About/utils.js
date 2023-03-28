f5e00ffa29c619696e5371cf62c81f9f944be3e5
Christian <cphvb91@gmail.com>
2023-03-28 21:01:20 +0200

    initial commit

---
+++ About/utils.js
@@ -0,0 +1,48 @@
+// Map number x from range [a, b] to [c, d]
+import imagesLoaded from 'imagesloaded'
+
+
+// Linear interpolation
+const lerp = (a, b, n) => (1 - n) * a + n * b;
+
+// Gets the mouse position
+const getMousePos = e => {
+    return { 
+        x : e.clientX, 
+        y : e.clientY 
+    };
+};
+
+const calcWinsize = () => {
+    return {width: window.innerWidth, height: window.innerHeight};
+};
+
+const getRandomInteger = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+// Preload images
+const preloadImages = (selector = 'img') => {
+    return new Promise((resolve) => {
+        imagesLoaded(document.querySelectorAll(selector), {background: true}, resolve);
+    });
+};
+
+// Preload fonts
+const preloadFonts = ([id]) => {
+    return new Promise((resolve) => {
+        WebFont.load({
+            google: {
+                families: id
+            },
+            active: resolve
+        });
+    });
+};
+
+export {
+    lerp,
+    getMousePos,
+    calcWinsize,
+    getRandomInteger,
+    preloadImages,
+    preloadFonts
+};
